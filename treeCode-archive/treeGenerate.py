15d2d12a3d92d5adf08e5b68e3ca9fc0f0652fbb
Nhan Tran <nhan.v.tran@gmail.com>
2019-05-31 21:04:40 -0500

    add the old code

---
+++ treeCode-archive/treeGenerate.py
@@ -0,0 +1,210 @@
+import ROOT
+
+# Define array
+
+arr = [];
+
+arr.append( ["Truong Kim Dien"    , ""                              , ""                                     , ""] );
+arr.append( ["Nguyen Thi Thuan"   , ""                              , ""                                     , "", "droppedImage_14.png"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( [""                   ,"Luu Ngoc Thanh"                 , ""                                     , ""] );
+arr.append( ["--"                 , "Truong Thi Mai"                , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "John Brosnan"                         , ""] );
+arr.append( [""                   , "--"                            , "Kim-Anh Brosnan [28.02.1966]"         , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Riley Brosnan [11.11.1996]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Abigail Brosnan [20.01.1999]"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Scott Larsen"                         , ""] );
+arr.append( [""                   , "--"                            , "Kim-Loan Larsen [09.05.1969]"         , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Calvert Larsen [06.08.1996]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Christian Larsen [31.05.1997]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Caelen Larsen [24.03.2002]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Virginia Larsen [20.04.2004]"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Dean Wence"                           , ""] );
+arr.append( [""                   , "--"                            , "Kimberley Wence [01.05.1970]"         , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Kira Wence [28.10.1998]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Ryan Wence [16.12.2000]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Brayden Wence [15.09.2004]"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Binh Luu [20.09.1971]"                , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Sean Gerstle [03.12.1963]"            , ""] );
+arr.append( [""                   , "--"                            , "Vivian Gerstle [25.12.1975]"          , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Sean Gerstle Jr."] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Jennifer Luu [04.12.1981]"            , ""] );
+arr.append( [""                   , ""                              , "Caitlin McIntyre"                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Rafferty Luu-McIntyre"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Scott Luu [19.09.1984]"               , ""] );
+arr.append( [""                   , ""                              , "Yuan He"                              , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( [""                   , "Nguyen Van Xuyen [02.11.1946 - 21.05.1968]"                 , ""                                     , ""] );
+arr.append( [""                   , "  -- Vuong Dinh Thanh [15.05.1936]" , ""                                     , ""] );
+arr.append( ["--"                 , "Truong Thi Quit [02.10.1947]"  , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Nguyen Quoc Cuong [02.04.1968]"       , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( ["--"                 , "Truong Duy Thom (dec.)"        , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( ["--"                 , "Truong Duy A"                  , ""                                     , ""] );
+arr.append( [""                   , "Phang Thi Ai Cuc"              , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Tim Fischer"                          , ""] );
+arr.append( [""                   , "--"                            , "Linda Fischer"                        , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Hailey Fischer"] );
+arr.append( [""                   , ""                              , "--"                                   , "Miah Fischer"] );
+arr.append( [""                   , ""                              , "--"                                   , "Jacob Fischer"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Steven Duy Truong (dec.)"                    , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( [""                   , "Ho Dai Son [10.12.1954]"       , ""                                     , ""] );
+arr.append( ["--"                 , "Truong Thi Chanh [02.10.1952]" , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Ho Dai Long [24.01.1981]"             , ""] );
+arr.append( [""                   , ""                              , "Nguyen Hoang Nhu Trang [22.06.1980]"  , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Ho Hoang Anh-Thu [18.10.2008]"] );
+arr.append( [""                   , ""                              , "--"                                   , "-placeholder-"] );
+arr.append( [""                   , ""                              , "--"                                   , "-placeholder-"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Van Phu Huan [20.01.1983]"            , ""] );
+arr.append( [""                   , "--"                            , "Ho Thi Duy Thao [23.03.1983]"         , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Ho Thi Duy An, Kyle [02.11.1991]"     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( [""                   , "Tran Phuoc Vinh"               , ""                                     , ""] );
+arr.append( ["--"                 , "Truong Thi Dao"                , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Tran Phuoc Huy"                       , ""] );
+arr.append( [""                   , ""                              , "Nguyen Dao Thuy Anh"                  , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Aidan Tran"] );
+arr.append( [""                   , ""                              , "--"                                   , "-placeholder-"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Tran Phuoc Vy"                        , ""] );
+arr.append( [""                   , ""                              , "Pham Thi Hong Thuy"                   , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Tran Phuoc Ky Lam"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Nguyen Si Hoang Tu"                   , ""] );
+arr.append( [""                   , "--"                            , "Tran Phuoc Ca-May"                    , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "-placeholder-"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( [""                   , "Arthur Santerre"               , ""                                     , ""] );
+arr.append( ["--"                 , "Truong Thi Man"                , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "Josh Ward"                            , ""] );
+arr.append( [""                   , "--"                            , "Christina Ward [12.12.1977]"      , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Livia Ward"] );
+arr.append( [""                   , ""                              , "--"                                   , "Samantha Ward"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Peter Santerre [04.08.1979]"          , ""] );
+arr.append( [""                   , ""                              , "Merideth Santerre [16.05.1979]"       , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Caden Santerre [03.01.2009]"] );
+arr.append( [""                   , ""                              , "--"                                   , "Grace Santerre [23.02.2010]"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( [""                   , "Tran Dai Kiem Ngoc [08.12.1949]", ""                                     , ""] );
+arr.append( ["--"                 , "Truong Thi Tuyet [15.07.1958]" , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Nhan Tran [15.07.1983]"               , ""] );
+arr.append( [""                   , ""                              , "Cancan Ma [13.03.1983]"               , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Parker Tran [18.12.2011]"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Matthew Brinker [30.09.1983]"         , ""] );
+arr.append( [""                   , ""                              , "Tammy Tran [26.03.1985]"              , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , ""                              , "--"                                   , "Molly Tran Brinker [20.10.2010]"] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+
+arr.append( ["--"                 , "Truong Du Xe"                  , ""                                     , ""] );
+arr.append( [""                   , "Nguyen Thi Hoang Sa"           , ""                                     , ""] );
+arr.append( [""                   , ""                              , ""                                     , ""] );
+arr.append( [""                   , "--"                            , "Kha Duy Truong"                       , ""] );
+arr.append( [""                   , "--"                            , "Phi Duy Truong"                       , ""] );
+
+elements = len(arr);
+textSize = 22;
+actualTextSize = textSize;
+columns = 4;
+columnWidth = 120;
+xoffset = 25;
+
+stepH = float(1)/float(elements+4);
+stepW = float(1)/float(columns+1);
+
+Ch = textSize * (elements+4);
+Cw = columnWidth*(columns+1) + 10*xoffset;
+
+C = ROOT.TCanvas("C","C",Cw,Ch);
+
+
+print "elements = ", elements
+
+def fillTS(ts,i):
+    
+
+    for j in range(len(arr[i])):
+        if arr[i][j] == "" or arr[i][j] == None:
+            continue;
+        elif arr[i][j] == "--":
+#            ts[i][j].SetTextFont(103);
+#            ts[i][j].SetTextSizePixels(actualTextSize);            
+#            ts[i][j].DrawLatex(float(xoffset)/float(Cw)+stepW*(j),1-stepH*(i+2),"#rightarrow");  
+
+            line = ROOT.TArrow();
+            #line.SetNDC();
+            line.SetLineColor(ROOT.kCyan + 3);
+            line.SetLineStyle(1);
+            line.SetArrowSize(0.001);            
+            line.DrawArrow(float(xoffset)/float(Cw)+stepW*(j)+(stepW/2), 1-stepH*(i+1.75), float(xoffset)/float(Cw)+stepW*(j)+(7*stepW/8), 1-stepH*(i+1.75));
+            
+        elif ".png" in arr[i][j]:
+            print "time to draw!"
+#            img = ROOT.TImage.Open("images/"+arr[i][j]);
+#            tmppad = ROOT.TPad("tmppad","tmppad",float(xoffset)/float(Cw)+stepW*(j)
+#                                                ,1-stepH*(i+4)
+#                                                ,float(xoffset)/float(Cw)+stepW*(j+1)
+#                                                ,1-stepH*(i));
+#            tmppad.cd();
+#            tmppad.Draw();
+#            img.Draw();
+
+        else:
+            #print "in here",arr[i][j]
+            #print "stepW*(i+1) = ",stepW*(j+1),", 1-stepH*(j+1) = ",1-stepH*(i+1)
+            ts[i][j].SetTextFont(43);
+            ts[i][j].SetTextSizePixels(textSize-4);            
+            ts[i][j].DrawLatex(float(xoffset)/float(Cw)+stepW*(j),1-stepH*(i+2),arr[i][j]);            
+            #ts[i][j].DrawLatex(0.5,0.5,"hi");
+
+if __name__ == '__main__':
+
+
+    C.cd();
+     
+    ts = []
+    for i in range(elements):
+        print i
+        ts.append( [ROOT.TLatex()]*len(arr[i]) );
+        fillTS(ts,i)
+
+    C.SaveAs("test.pdf");
